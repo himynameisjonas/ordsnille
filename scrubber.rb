c451e33512d51456c7fd93cbb01b387355fcaeb1
Jonas Brusman <jonas@brusman.se>
2022-01-05 21:26:35 +0100

    export a shuffled js array

--- scrubber.rb
+++ scrubber.rb
@@ -2,10 +2,14 @@ require "bundler/setup"
 Bundler.require
 
 words = File.readlines('raw_words.txt')
-open('words.txt', 'w') do |f|
-  words.each do |word|
-    next if word.include?('-')
-    next if word.include?(' ')
-    f.puts word
-  end
+words.map! do |word|
+  next if word.include?('-')
+  next if word.include?(' ')
+  "'#{word.strip}'"
+end
+
+open('src/words.js', 'w') do |f|
+  f.puts "export default ["
+  f.puts words.compact.shuffle(random: Random.new(1)).join(',')
+  f.puts "];"
 end
