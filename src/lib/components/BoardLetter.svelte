124acbd1b8ac139eb8947f870f11ced118799a08
Jonas Brusman <jonas@brusman.se>
2022-01-06 10:34:05 +0100

    Some animations

--- src/lib/components/BoardLetter.svelte
+++ src/lib/components/BoardLetter.svelte
@@ -1,15 +1,25 @@
 <script>
   export let letter = "";
   export let hint = "";
+  let animate = false;
+
+  $: {
+    if (letter != "") {
+      animate = true;
+      setTimeout(() => {
+        animate = false;
+      }, 500);
+    }
+  }
 
   function classFromHint(hint) {
     switch (hint) {
       case 2:
-        return "text-green-900 bg-green-400";
+        return "border-green-500 text-green-900 bg-green-400";
       case 1:
-        return "text-yellow-900 bg-yellow-400";
+        return "border-yellow-500 text-yellow-900 bg-yellow-400";
       case 0:
-        return "text-gray-600 bg-gray-400";
+        return "border-gray-500 text-gray-600 bg-gray-400";
       default:
         return "bg-gray-50";
     }
@@ -17,7 +27,9 @@
 </script>
 
 <div
-  class="border-2 m-1 h-16 w-16 text-5xl uppercase font-bold flex items-center justify-center {classFromHint(
+  class:animate-pop={animate}
+  class:border-gray-300={letter}
+  class="transition-colors border-2 m-1 h-16 w-16 text-5xl uppercase font-bold flex items-center justify-center {classFromHint(
     hint
   )}"
 >
