124c607ad0e707823d7b58caf2ed1ac960e3e7ad
Jonas Brusman <jonas@brusman.se>
2022-01-06 23:30:18 +0100

    Bullet instructions

--- src/lib/components/Instructions.svelte
+++ src/lib/components/Instructions.svelte
@@ -9,15 +9,22 @@
 </script>
 
 <div class="m-auto w-[65ch] max-w-full p-5">
-  <h2 class="text-2xl font-bold">Försök gissa order på 6 försök</h2>
-  <p>
-    Varje försök ger ledtrådar till det rätta svaret, <span
+  <h2 class="text-2xl font-bold">Försök gissa dagens ord!</h2>
+  <ul class="list-disc list-inside">
+  <li class="mb-5">
+    Du har 6 försök och varje försök ger nya ledtrådar till det rätta svaret.
+  </li>
+  <li class="mb-5">
+    <span
       class="bg-green-300 p-1 text-green-700 font-bold uppercase">gröna</span
     >
-    bokstäver betyder att det är rätt bokstav på rätt plats, en
-    <span class="bg-orange-200 p-1 text-orange-600 font-bold uppercase">orange</span> bokstav betyder
+    bokstäver betyder att det är rätt bokstav på rätt plats.
+  </li>
+  <li class="mb-5">
+      En <span class="bg-orange-200 p-1 text-orange-600 font-bold uppercase">orange</span> bokstav betyder
     att den bokstaven finns med i det rätta svaret men att den är på fel plats.
-  </p>
+  </li>
+  </ul>
   <button on:click={start} class="w-full bg-green-500 text-white font-bold p-2 rounded my-5"
     >Starta</button
   >
