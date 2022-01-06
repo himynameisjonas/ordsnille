ab2ccf0890be0df87441cfa76374722a3821c4b6
Jonas Brusman <jonas@brusman.se>
2022-01-06 21:48:44 +0100

    lint test files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,6 @@ module.exports = {
   root: true,
   extends: ["eslint:recommended", "prettier"],
   plugins: ["svelte3"],
-  overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
   parserOptions: {
     sourceType: "module",
     ecmaVersion: 2020,
@@ -12,4 +11,15 @@ module.exports = {
     es2017: true,
     node: true,
   },
+  overrides: [
+    { files: ["*.svelte"], processor: "svelte3/svelte3" },
+    {
+      files: ["*.test.js", "*.spec.js"],
+      plugins: ["jest"],
+      extends: ["plugin:jest/recommended", "plugin:jest/style"],
+      env: {
+        "jest/globals": true,
+      },
+    },
+  ],
 };
