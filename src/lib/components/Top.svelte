b64ffeaabbbc8758174b28ec71ac1cdf935491aa
Jonas Brusman <jonas@brusman.se>
2022-01-06 18:19:13 +0100

    Game name

--- src/lib/components/Top.svelte
+++ src/lib/components/Top.svelte
@@ -3,6 +3,6 @@
 
 <div class="py-2 bg-gray-100 flex justify-center items-center border-b-4 border-b-fuchsia-400">
   <h1 class="text-5xl uppercase font-bold text-gray-600">
-    W<span class="text-emerald-600">ö</span>rdle
+    <span class="text-emerald-600">Ord</span>snille
   </h1>
 </div>
