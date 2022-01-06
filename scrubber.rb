9b2569b9b2f18c9000656d266767c507cfc05206
Jonas Brusman <jonas@brusman.se>
2022-01-06 09:02:36 +0100

    more words

--- scrubber.rb
+++ scrubber.rb
@@ -1,14 +1,20 @@
 require "bundler/setup"
 Bundler.require
-
-words = File.readlines('raw_words.txt')
+words = []
+words |= File.readlines('raw_words.txt')
+words |= File.readlines('dictionary.txt')
+puts words.size
 words.map! do |word|
   next if word.include?('-')
   next if word.include?(' ')
-  "'#{word.strip}'"
+  next if word.include?('é')
+  next if /[[:upper:]]/.match(word)
+  word.strip!
+  next unless word.length == 5
+  "'#{word}'"
 end
 
-open('src/words.js', 'w') do |f|
+open('src/lib/words.js', 'w') do |f|
   f.puts "export default ["
   f.puts words.compact.shuffle(random: Random.new(1)).join(',')
   f.puts "];"
