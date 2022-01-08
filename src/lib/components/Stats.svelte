805b3e11f2906b83be4a57f1d0650633d5c1e7ec
Jonas Brusman <jonas@brusman.se>
2022-01-08 21:16:09 +0100

    More stats!

--- src/lib/components/Stats.svelte
+++ src/lib/components/Stats.svelte
@@ -1,36 +1,61 @@
 <script>
   import game from "$lib/stores/game.js";
-  import { stats, graphs } from "$lib/stores/stats";
+  import { stats, graphs, plays, winPercentage } from "$lib/stores/stats";
   import { todaysWord } from "$lib/stores/word.js";
 </script>
 
 <div class="mx-auto mt-5 w-[65ch] max-w-full px-5 text-gray-700 mb-auto">
   <h2 class="text-3xl mb-4 text-center font-abril">Resultat</h2>
-  <p class="mb-5">
-    Du {#if $stats.lastStatus == "success"}lyckades{:else}misslyckades{/if} i senaste spelet. Det rätta
-    ordet var
-    <span class="bg-green-300 p-1 text-green-700 font-bold uppercase">{$stats.lastSolution}</span>.
+
+  <p class="mb-5 bg-white rounded-lg border shadow p-4">
+    Senaste ordet du spelade var <span class="bg-green-300 p-1 text-green-700 font-bold uppercase"
+      >{$stats.lastSolution}</span
+    >
+    och du {#if $stats.lastStatus == "success"}gissade rätt{:else}hann inte gissa rätt{/if}.
   </p>
 
-  <h3 class="text-xl mb-1 text-center font-abril">Antal gissningar i vunna spel</h3>
-  <table>
-    <tbody>
-      {#each $graphs as score}
-        <tr class="border-b">
-          <td class="font-bold pr-1">{score.points}</td>
-          <td width="100%">
-            <div class="flex items-center">
-              <div class="h-4 bg-green-300 rounded-r" style="width: {score.percentage}%" />
-              <div class="ml-1 text-sm">{score.guesses}</div>
-            </div>
-          </td>
-        </tr>
-      {/each}
-    </tbody>
-  </table>
+  <div class="mb-5 bg-white rounded-lg border shadow p-4">
+    <h3 class="text-xl mb-1 text-center font-abril">Statistik</h3>
+    <div class="flex text-center flex-wrap">
+      <div class="w-1/2 mb-4">
+        <strong class="text-2xl block">{$plays}</strong>
+        <span class="text-xs uppercase">antal spel</span>
+      </div>
+      <div class="w-1/2 mb-4">
+        <strong class="text-2xl block">{$winPercentage}</strong>
+        <span class="text-xs uppercase">% vinster</span>
+      </div>
+      <div class="w-1/2 mb-4">
+        <strong class="text-2xl block">{$stats.currentStreak}</strong>
+        <span class="text-xs uppercase">nuvarande segerserie</span>
+      </div>
+      <div class="w-1/2 mb-4">
+        <strong class="text-2xl block">{$stats.maxStreak}</strong>
+        <span class="text-xs uppercase">högsta segerserie</span>
+      </div>
+    </div>
+  </div>
+  <div class="mb-5 bg-white rounded-lg border shadow p-4">
+    <h3 class="text-xl mb-1 text-center font-abril">Antal vunna spel per antal gissningar</h3>
+    <table>
+      <tbody>
+        {#each $graphs as score}
+          <tr class="border-b">
+            <td class="font-bold pr-1">{score.points}</td>
+            <td width="100%">
+              <div class="flex items-center">
+                <div class="h-4 bg-green-300 rounded-r" style="width: {score.percentage}%" />
+                <div class="ml-1 text-xs">{score.guesses}</div>
+              </div>
+            </td>
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  </div>
 </div>
 
-<div class="px-5 my-auto">
+<div class="p-5 my-auto">
   {#if $game.solution != $todaysWord}
     <button
       on:click={game.restart}
--- src/lib/stores/stats.js
+++ src/lib/stores/stats.js
@@ -4,6 +4,8 @@ function defaultValues() {
   return {
     scores: Array(6).fill(0),
     failures: 0,
+    currentStreak: 0,
+    maxStreak: 0,
   };
 }
 
@@ -29,11 +31,15 @@ export const stats = (function () {
         $stats.scores[game.boardIndex] = $stats.scores[game.boardIndex] + 1;
         $stats.lastSolution = game.solution;
         $stats.lastStatus = "success";
+        $stats.currentStreak = ($stats.currentStreak || (game.lastStatus == "success" ? 1 : 0)) + 1;
+        $stats.maxStreak = Math.max($stats.currentStreak, $stats.maxStreak || 0);
+
         return $stats;
       }),
     logFailure: (game) =>
       update(($stats) => {
         $stats.failures = $stats.failures + 1;
+        $stats.currentStreak = 0;
         $stats.lastSolution = game.solution;
         $stats.lastStatus = "failure";
         return $stats;
@@ -41,13 +47,25 @@ export const stats = (function () {
   };
 })();
 
-export const graphs = derived(stats, ($stats) => {
-  let totalWins = $stats.scores.reduce((score, sum) => score + sum, 0);
+export const totalWins = derived(stats, ($stats) => {
+  return $stats.scores.reduce((score, sum) => score + sum, 0);
+});
+
+export const graphs = derived([totalWins, stats], ([$totalWins, $stats]) => {
   return $stats.scores.map((score, index) => {
     return {
       points: index + 1,
       guesses: score,
-      percentage: (score / totalWins) * 100,
+      percentage: (score / $totalWins) * 100,
     };
   });
+});
+
+export const plays = derived([totalWins, stats], ([$totalWins, $stats]) => {
+  return $totalWins + $stats.failures;
+});
+
+export const winPercentage = derived([totalWins, plays], ([$totalWins, $plays]) => {
+  let percentage = ($totalWins / $plays) * 100;
+  return Math.round((percentage + Number.EPSILON) * 100) / 100;
 });
