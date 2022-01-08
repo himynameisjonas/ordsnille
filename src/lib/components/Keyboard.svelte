1eac06dc889b3a7fd61169e669140afaee82710a
Jonas Brusman <jonas@brusman.se>
2022-01-08 22:29:15 +0100

    some icons

--- src/lib/components/Keyboard.svelte
+++ src/lib/components/Keyboard.svelte
@@ -45,7 +45,20 @@
           on:click={() => dispatch("delete")}
           class="h-14 rounded m-[2px] bg-slate-400 uppercase text-white font-bold w-20 flex items-center justify-center"
         >
-          ⌫
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M12 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2M3 12l6.414 6.414a2 2 0 001.414.586H19a2 2 0 002-2V7a2 2 0 00-2-2h-8.172a2 2 0 00-1.414.586L3 12z"
+            />
+          </svg>
         </button>
       {/if}
     </div>
--- src/lib/components/Top.svelte
+++ src/lib/components/Top.svelte
@@ -3,9 +3,20 @@
   <h1 class="text-5xl uppercase text-gray-500 font-abril mx-auto">
     <span class="text-green-500">Ord</span>snille
   </h1>
-  <a
-    href="/instruktioner"
-    class="text-gray-500 rounded-full border w-6 h-6 mr-2 border-current flex justify-center items-center"
-    >?</a
+  <a href="/instruktioner" class="text-gray-500 mr-2"
+    ><svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg></a
   >
 </div>
