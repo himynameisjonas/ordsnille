b6c508e606626b3f6f8f6e0291697e885e4ba892
Jonas Brusman <jonas@brusman.se>
2022-01-09 15:18:33 +0100

    refesh app on logo click

--- src/lib/components/Top.svelte
+++ src/lib/components/Top.svelte
@@ -1,6 +1,15 @@
+<script>
+  function refresh() {
+    window.location.reload();
+  }
+</script>
+
 <div class="py-0.5 bg-green-100 flex justify-center items-center border-b-[3px] border-b-green-400">
   <span class="flex w-6 h-6" />
-  <h1 class="text-5xl uppercase text-gray-500 font-abril mx-auto">
+  <h1
+    on:click={refresh}
+    class="hover:cursor-pointer text-5xl uppercase text-gray-500 font-abril mx-auto"
+  >
     <span class="text-green-500">Ord</span>snille
   </h1>
   <a href="/instruktioner" class="text-gray-500 mr-2"
