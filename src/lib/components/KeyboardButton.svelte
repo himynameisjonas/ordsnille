586546955317dae211fa2c14f3545a4bcbed0e5c
Jonas Brusman <jonas@brusman.se>
2022-01-22 17:44:33 +0100

    fix keyboard letter text color

--- src/lib/components/KeyboardButton.svelte
+++ src/lib/components/KeyboardButton.svelte
@@ -6,19 +6,19 @@
   let classes = "";
 
   $: if ($correct.has(key)) {
-    classes = "bg-sky-400 text-sky-800";
+    classes = "bg-sky-400 text-sky-700";
   } else if ($present.has(key)) {
     classes = "bg-yellow-300 text-yellow-700";
   } else if ($absent.has(key)) {
     classes = "bg-gray-500 text-gray-400";
   } else {
-    classes = "bg-slate-400";
+    classes = "bg-slate-400 text-sky-50";
   }
 </script>
 
 <button
   on:click
-  class="h-14 rounded m-[2px] uppercase text-sky-50 font-bold w-10 flex items-center justify-center {classes}"
+  class="h-14 rounded m-[2px] uppercase font-bold w-10 flex items-center justify-center {classes}"
 >
   {key}
 </button>
