4f0078454fbb78ea33f140c968f0d5694da8ddcc
Jonas Brusman <jonas@brusman.se>
2025-06-06 11:11:33 +0200

    Update scrubber logic

--- scraper/scrubber.rb
+++ scraper/scrubber.rb
@@ -3,7 +3,7 @@ Bundler.require
 words = []
 words |= File.readlines('saol2018clean.txt').map { _1.split(",")[1]}
 # words |= File.readlines('raw_words.txt')
-words |= File.readlines('swe_wordlist_5.txt')
+# words |= File.readlines('swe_wordlist_5.txt')
 blocklist = File.readlines('blocklist.txt').map &:strip
 # words |= File.readlines('dictionary.txt')
 puts words.size
@@ -13,7 +13,6 @@ words.map! do |word|
   next if word.include?('.')
   next if word.include?(':')
   next if word.include?('é')
-  next if /[[:upper:]]/.match(word)
   word.strip!
   next unless word.length == 5
   next if blocklist.include?(word)
@@ -27,7 +26,7 @@ end
 # end
 
 open('scrubbed.txt', 'w') do |f|
-  words.compact.uniq.sort.each {
-    f.puts _1.gsub("'","")
+  words.compact.uniq.shuffle.each {
+    f.puts '"' + _1.downcase.gsub("'","") + '",'
   }
 end
