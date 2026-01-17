cd70baadbbc3f70ffda5aeb739dc046e8739a0f0
Jonas Brusman <jonas@brusman.se>
2026-01-17 12:31:40 +0100

    use static adapter for cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from "@sveltejs/adapter-auto";
+import autoAdapter from "@sveltejs/adapter-auto";
+import staticAdapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,7 +7,7 @@ const config = {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter: process.env.CF_PAGES ? staticAdapter() : autoAdapter(),
   },
 };
 
